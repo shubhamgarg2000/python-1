ea5614cc64389f52a348b327fe70f78f52937b77
Shubham garg <42842217+shubhamgarg2000@users.noreply.github.com>
2019-10-02 00:14:37 +0530

    ADDED SALARY ALSO

--- 1.py
+++ 1.py
@@ -5,6 +5,7 @@ my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
+my_salary = '15000'
 
 print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
@@ -12,6 +13,8 @@ print(f"He's {my_weight} pounds heavy.")
 print("Actually that's not too heavy.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"Let's talk about {my_salary}.")
+
 
 # this line is tricky, try to get it exactly right
 total = my_age + my_height + my_weight
